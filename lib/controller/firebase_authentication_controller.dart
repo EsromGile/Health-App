f93da11c71a7ab344503a88dcc3c1246ed86d4c3
Lucas Wolf <lwolf5@uco.edu>
2022-10-21 11:52:41 -0500

    added signin/signout methods

--- lib/controller/firebase_authentication_controller.dart
+++ lib/controller/firebase_authentication_controller.dart
@@ -1,3 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class FirebaseAuthenticationController {
   // pass login info to firebase to verify login
+  static Future<User?> signIn(
+      {required String email, required String password}) async {
+    UserCredential userNameAndPassword = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    return userNameAndPassword.user;
+  }
+
+  static Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 }
