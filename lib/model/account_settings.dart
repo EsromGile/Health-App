c1320b5e94ced299eb186aea089f4fc2288b1755
Lucas Wolf <lwolf5@uco.edu>
2022-11-14 11:23:39 -0600

    account settings added atttributes

--- lib/model/account_settings.dart
+++ lib/model/account_settings.dart
@@ -1,10 +1,46 @@
 import 'package:health_app/model/user_account.dart';
-
+enum DocKeyAccountSettings {
+  docId,
+  user,
+  uid,
+  uploadRate,
+  collectionFrequency
+}
 class AccountSettings {
-  final UserAccount user;
-  int uploadRate = 30;          // every 30 minutes
-  int collectionFrequency = 5;  // every 5 minutes 
-  bool isDarMode = true;        //😎
+  String? docId; //firestore generated id
+  late UserAccount user;
+  late String uid;
+  late int uploadRate;          // every 30 minutes
+  late int collectionFrequency;  // every 5 minutes 
 
-  AccountSettings({required this.user});
+  AccountSettings({
+    required this.user,
+    this.docId,
+    this.uid = '',
+    this.uploadRate = 30,
+    this.collectionFrequency = 5,
+  });
+
+  Map<String, dynamic> toFirestoreDoc() {
+    return {
+      DocKeyAccountSettings.user.name: user,
+      DocKeyAccountSettings.docId.name: docId,
+      DocKeyAccountSettings.uid.name: uid,
+      DocKeyAccountSettings.uploadRate.name: uploadRate,
+      DocKeyAccountSettings.collectionFrequency.name: collectionFrequency,
+    };
+  }
+
+  static AccountSettings? fromFirestoreDoc({
+    required Map<String, dynamic> doc,
+    required String docId,
+  }) {
+    return AccountSettings(
+      docId: docId,
+      user: doc[DocKeyAccountSettings.user.name] ?? 'N/A',
+      uid: doc[DocKeyAccountSettings.uid.name] ?? 'N/A',
+      uploadRate: doc[DocKeyAccountSettings.uploadRate.name] ?? 'N/A',
+      collectionFrequency: doc[DocKeyAccountSettings.collectionFrequency.name] ?? 'N/A',
+    );
+  }
 }
---
+++ lib/viewscreen/view/iNormalize.dart
@@ -0,0 +1,3 @@
+abstract class Normalize {
+  void normalizeData();
+}
